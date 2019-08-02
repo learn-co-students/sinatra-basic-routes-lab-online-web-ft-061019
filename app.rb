8f8d9972e78d39249351e8e7aef9ce321fea2834
jharrel <harrel.jason@gmail.com>
2019-08-02 14:58:25 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Jason"
+  end
+
+  get '/hometown' do
+    "My hometown is Portland, Oregon"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Understatement by New Found Glory"
+  end
 end
